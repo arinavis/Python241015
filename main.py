9f50b2f7de7b1341045e2135e0c5d4213e0542ed
arinavis <avisnevskaja@gmail.com>
2024-10-16 22:01:33 +0300

    Employees system program. Added filtering by gender

--- main.py
+++ main.py
@@ -3,37 +3,40 @@
 
 employees = [
     [
-        'Lukas K.', 35, 3000
+        'Lukas K.', 35, 3000, 1, 'vyras'
      ],
     [
-        'Edita B.', 38, 1500
+        'Edita B.', 38, 1500, 0.5, 'moteris'
     ],
     [
-        'Laura U.', 45, 2500
+        'Laura U.', 45, 2500, 1, 'moteris'
     ],
     [
-        'Arina V.', 28, 2200
+        'Arina V.', 28, 2200, 1, 'moteris'
     ],
     [
-        'Tautvydas V.', 30, 1800
+        'Tautvydas V.', 30, 1800, 0.75, 'vyras'
     ]
 ]
 
 def printInfo():
+    print('------------------------------------------------------')
     print('Išsirinkite veiksmą, įrašydami jo eilės numerį:')
     print('1. Darbuotojų sąrašas')
     print('2. Įvesti naują darbuotoją')
     print('3. Redaguoti darbuotojo duomenis')
     print('4. Pašalinti darbuotoją iš sąrašo')
-    print('5. Išeiti iš programos')
+    print('5. Filtruoti duomenis')
+    print('6. Išeiti iš programos')
     print('------------------------------------------------------')
 
 def employeesInfo(emp, num = 1):
-    print(f'{num}. Darbuotojas {emp[0]}, amžius - {emp[1]} metai, DU - {emp[2]}')
+    print(f'{num}. Darbuotojas {emp[0]}, amžius - {emp[1]} metai, DU - {emp[2]}, užimtumas: {emp[3]}, lytis: {emp[4]}')
 
-def printEmployees():
+def printEmployees(lst = employees):
+    print('---DARBUOTOJŲ SĄRAŠAS---')
     num = 1
-    for emp in employees:
+    for emp in lst:
         employeesInfo(emp, num)
         num += 1
 
@@ -44,7 +47,11 @@ def addEmployees():
     empAge = int( input() )
     print('Darbuotojo DU')
     empSalary = int( input() )
-    employees.append([empName, empAge, empSalary])
+    print('Darbuotojo užimtumas')
+    empEmployment = float(input())
+    print('Darbuotojo lytis')
+    empGender = input()
+    employees.append([empName, empAge, empSalary, empEmployment, empGender])
 
 def editEmployees():
     print('Įveskite darbuotojo eilės numerį, kurio informaciją norite redaguoti')
@@ -57,7 +64,11 @@ def editEmployees():
     empAge = int(input())
     print('Darbuotojo DU')
     empSalary = int(input())
-    employees[ed - 1] = [empName, empAge, empSalary]
+    print('Darbuotojo užimtumas')
+    empEmployment = float(input())
+    print('Darbuotojo lytis')
+    empGender = input()
+    employees[ed - 1] = [empName, empAge, empSalary, empEmployment, empGender]
 
 def removeEmployees():
     print('Įveskite darbuotojo eilės numerį, kurio informaciją norite pašalinti')
@@ -66,22 +77,50 @@ def removeEmployees():
 
 print('---Skyriaus darbuotojų valdymo sistema---')
 
-# printEmployees()
-
 while True:
     printInfo()
     option = int( input() )
     match option:
         case 1:
             printEmployees()
+            continue
         case 2:
             addEmployees()
-            printEmployees()
         case 3:
             editEmployees()
-            printEmployees()
+            # printEmployees()
         case 4:
             removeEmployees()
-            printEmployees()
+            # printEmployees()
         case 5:
+            print('Pasirinkite pagal ką filtruoti (įrašykite eilės numerį):')
+            print('1. Pagal užimtumą')
+            print('2. Pagal lytį')
+            filterNum = int(input())
+            if filterNum == 1:
+                print('Įveskite eteto dydį("PL" - pilnas / "NPL" - nepilnas / "DPL" - daugiau negu pilnas)')
+                flEmpl = input()
+                PL = 1
+                # tempList0 =[]
+                for emp in employees:
+                    if emp[3] == flEmpl:
+
+            if filterNum == 2:
+                print('Įveskite lytį (vyras/moteris)')
+                filterGen = input().lower()
+                # count = 0
+                tempList = []
+                for emp in employees:
+                    if emp[4] == filterGen:
+                        # count += 1
+                        # employeesInfo(emp, count)
+                        tempList.append(emp)
+                #some kind of woodo magic
+                lst = sorted(tempList, key=lambda x: x[0])
+                print('Išfiltruoti duomenys pagal lytį ir surušiuota pagal vardą (A-Z)')
+                printEmployees(lst)
+
+        case 6:
                 exit(1)
+    printEmployees()
+
